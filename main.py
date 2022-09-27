7019875013c54783f3d20892fe1324ca63dc6dba
MannerlyMeat <ut.eugene@gmail.com>
2022-09-27 13:33:30 +0300

    Полностью рабочий парсер данных

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 import sqlite3
+import re
 from bs4 import BeautifulSoup as bs
 
 # conn = MySQLdb.connect("127.0.0.1", "root", "", "roulette_movies")
@@ -19,13 +20,11 @@ def get_count_pages():
 
 def get_fnames():
     for pages in range(int(get_count_pages())):
-        print(pages)
         active_url = ("https://www.metacritic.com/browse/movies/score/metascore/all/filtered?sort=desc&page={}".format(pages))
         requestSite = requests.get(active_url, headers=headers, allow_redirects=True)
         soup = bs(requestSite.text, "html.parser")
         film_names = soup.find_all('a', class_="title")
-        rating = soup.find_all('div', class_="metascore_w large movie positive")
-        #print(rating)
+        rating = soup.find_all('div', attrs = {'class':re.compile("metascore_w large movie")})
         fill_database(film_names, rating)
 
 
@@ -43,14 +42,13 @@ def create_sqlite_DB():
 
 def check_database(film_name):
     cur = conn.cursor()
-    cur.execute('select exists(select * from movies where movie_name = "{}")'.format(film_name))
+    cur.execute("""select exists(select * from movies where movie_name = "{}")""".format(film_name))
     res = cur.fetchone()
     return res[0]
 
 
 def insert_to_bd(movie_name: str, rating: int):
     cur = conn.cursor()
-    #print(movie_name, rating)
     text = r"""
                 INSERT INTO movies(movie_name, rating) 
                 VALUES('{movie_name}', {rating});
@@ -62,26 +60,30 @@ def insert_to_bd(movie_name: str, rating: int):
                 """.format(movie_name=movie_name, rating=rating))
     conn.commit()
 
+
 def delete_name_film(movie_name):
     cur = conn.cursor()
     cur.execute("DELETE FROM movies WHERE movie_name='{}'".format(movie_name))
     conn.commit()
     print(cur.fetchall())
 
+
 def fill_database(film_names, film_rating):
 
     for i in range(len(film_names)):
         sip = film_names[i].h3.string
+        sip = sip.replace('"', '')
         temp_bool = check_database(sip)
+        print(len(film_names))
+        print(len(film_rating))
         if not temp_bool:
-            insert_to_bd(film_names[i].h3.string, int(film_rating[i].string))
+            insert_to_bd(sip, int(film_rating[i].string))
             conn.commit()
     
 
 
-#count = soup.find_all('span', class_="title numbered")
-#count = len(count)
 
-#create_sqlite_DB()
+
+
 get_fnames()
-#all_Pages = get_count_pages()
+
